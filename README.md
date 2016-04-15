5430080f837858bdb1b431b8840aa3c090bc0041
Jimbo <kipperjim@gmail.com>
2016-04-15 17:57:20 +0100

    added logging to server add ci status to readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
+[![Build Status](https://travis-ci.org/kipperjim/kipper-calendar.svg?branch=master)](https://travis-ci.org/kipperjim/kipper-calendar)
 # kipper-calendar
 A month and day calendar built with Scala, LiftWeb and Bootstrap 3.
--- src/test/scala/kippercalendar/acceptance/server/WebServer.scala
+++ src/test/scala/kippercalendar/acceptance/server/WebServer.scala
@@ -1,8 +1,10 @@
 package kippercalendar.acceptance.server
 
 import im.mange.little.LittleServer
+import net.liftweb.common.Loggable
 
-class WebServer(port: Int) {
+class WebServer(port: Int) extends Loggable {
   new LittleServer(port, webAppPath = "src/test/webapp")
   println("### Started web server on port %d...".format(port))
+  logger.info("### Started web server on port %d...".format(port))
 }
